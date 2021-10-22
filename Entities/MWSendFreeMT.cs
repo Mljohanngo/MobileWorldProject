a7b11247a44d99fb87a8cb75e7657db78204f28a
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-10-21 21:55:16 -0500

    Add Send Free MT model

---
+++ Entities/MWSendFreeMT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class MWSendFreeMT
+   {
+        public long SubscriptionId { get; set; }
+        public string Action { get; set; }
+        public string Msisdn { get; set; }
+        public string BodyMessage { get; set; }
+        public string ProductId { get; set; }
+        public int HttpResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+        public int ResponseCode { get; set; }
+        public string ResultMessage { get; set; }
+    }
+}
