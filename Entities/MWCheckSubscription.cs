ef3353dfc71c4e8961218ea5124777b94f468cb7
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-10-21 21:43:55 -0500

    Add Check Subscription model

---
+++ Entities/MWCheckSubscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class MWCheckSubscription
+   {
+        public long SubscriptionId { get; set; }
+        public string Action { get; set; }
+        public string Msisdn { get; set; }
+        public string ProductId { get; set; }
+        public int HttpResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+        public int SubscriptionStatus { get; set; }
+        public string Message { get; set; }
+    }
+}
