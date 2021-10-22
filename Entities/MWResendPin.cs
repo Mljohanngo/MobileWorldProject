9a33f530925b5c5a7456e63cbfd6b12821fdbca5
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-10-21 21:32:03 -0500

    Add ResendPin Model

---
+++ Entities/MWResendPin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class MWResendPin
+   {
+        public long SubscriptionId { get; set; }
+        public string Action { get; set; }
+        public string Msisdn { get; set; }
+        public string ProductId { get; set; }
+        public int HttpResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+        public int ResponseCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
