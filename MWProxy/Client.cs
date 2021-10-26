6441021716b55474481485279d9d9406f27636d7
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-10-26 14:22:36 -0500

    Add Client class to handle all http requests with Mobile World

---
+++ MWProxy/Client.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWProxy
+{
+    public class Client
+    {
+        private readonly IConfiguration Configuration;
+        private readonly IHttpClientFactory _clientFactory;
+        public Client(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static object SendPin(object SendPinDto)
+        {
+            return null;
+        }
+    }
+}
