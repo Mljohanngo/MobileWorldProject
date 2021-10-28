6179714eb7e7f7fe9857d05b90f953ce52dc693f
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-10-28 01:00:17 -0500

    Update MWService to receive htto client and configs, and modified its constructor

--- MWProxy/MWService.cs
+++ MWProxy/MWService.cs
@@ -11,56 +11,51 @@ namespace MWProxy
 {
     public class MWService
     {
+        private readonly HttpClient _httpClient;
         private readonly IConfiguration _configuration;
-        public HttpClient Client { get; }
-        public MWService(IConfiguration configuration, HttpClient client)
+        public MWService(HttpClient client, IConfiguration configuration)
         {
-            client.BaseAddress = new Uri("https://api.github.com/");
-            // GitHub API versioning
-            client.DefaultRequestHeaders.Add("Accept",
-                "application/vnd.github.v3+json");
-            // GitHub requires a user-agent
-            client.DefaultRequestHeaders.Add("User-Agent",
-                "HttpClientFactory-Sample");
-
-            Client = client;
-
+            client.BaseAddress = new Uri(_configuration.GetValue<string>("MWConfig:HTTP:BaseUrl"));
+            var headerValue = $"{_configuration.GetValue<string>("MWConfig:Credentials:UserName")}:{_configuration.GetValue<string>("MWConfig:Credentials:PassWord")}";
+            client.DefaultRequestHeaders.Add($"{_configuration.GetValue<string>("MWConfig:HTTP:Header")}", 
+                $"{Convert.ToBase64String(Encoding.ASCII.GetBytes(headerValue))}");
+            _httpClient = client;
         }
 
-        public async Task<SendPinRequestDto> SendPinAsync(SendPinRequestDto Request)
+        public async Task<SendPinRequestDto> SendPinAsync(SendPinRequestDto Request, string config)
         {
 
-            var client = await Client.PostAsJsonAsync(Client.BaseAddress, new { test = "test"});
+            var client = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, new { test = "test"});
             return null;
         }
 
         public async Task<ReSendPinResponseDto> ReSendPinAsync(ReSendPinRequestDto Request)
         {
-            var client = await Client.PostAsJsonAsync(Client.BaseAddress, new { test = "test" });
+            var client = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, new { test = "test" });
             return null;
         }
 
         public async Task<ConfirmPinResponseDto> ConfirmPinAsync(ConfirmPinRequestDto Request)
         {
-            var client = await Client.PostAsJsonAsync(Client.BaseAddress, new { test = "test" });
+            var client = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, new { test = "test" });
             return null;
         }
 
         public async Task<CheckSubscriptionResponseDto> CheckSubscriptionAsync(CheckSubscriptionRequestDto Request)
         {
-            var client = await Client.PostAsJsonAsync(Client.BaseAddress, new { test = "test" });
+            var client = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, new { test = "test" });
             return null;
         }
 
         public async Task<CancelSubscriptionResponseDto> CancelSubscriptionAsync(CancelSubscriptionRequestDto Request)
         {
-            var client = await Client.PostAsJsonAsync(Client.BaseAddress, new { test = "test" });
+            var client = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, new { test = "test" });
             return null;
         }
 
         public async Task<SendFreeMTResponseDto> SendFreeMTAsync(SendFreeMTRequestDto Request)
         {
-            var client = await Client.PostAsJsonAsync(Client.BaseAddress, new { test = "test" });
+            var client = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress, new { test = "test" });
             return null;
         }
     }
