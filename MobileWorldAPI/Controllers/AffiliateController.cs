6924eff00fe62220107c05a9b7bc8e89be8130f0
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-05 15:52:52 -0600

    Add View Razor / Affiliate Controller

---
+++ MobileWorldAPI/Controllers/AffiliateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileWorldAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AffiliateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Index")]
+        public ActionResult Subscribe() 
+        {
+            // Logica del HIt y Subs en Pending
+            return Redirect("https://localhost:44393/Index");
+        }
+    }
+}
