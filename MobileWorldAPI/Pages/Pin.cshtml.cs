ceb5299e5200c215ace311d135c526a6d9ade07e
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-12 21:34:45 -0600

    Add validation pin logic

--- MobileWorldAPI/Pages/Pin.cshtml.cs
+++ MobileWorldAPI/Pages/Pin.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using DTO.MW;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MWProxy;
@@ -12,6 +13,8 @@ namespace MobileWorldAPI.Pages
     public class PinModel : PageModel
     {
         private readonly MWService _mWService;
+        private SendPinResponseDto _sendPinResponseDto;
+        private string clientIpAddress;
         public PinModel(MWService mWService)
         {
             _mWService = mWService;
@@ -20,13 +23,36 @@ namespace MobileWorldAPI.Pages
         [BindProperty]
         [Required(ErrorMessage = "OTP is required")]
         public long Otp { get; set; }
-        public void OnGet()
+        public void OnGet(SendPinResponseDto passedData)
         {
+            clientIpAddress = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "176.205.206.244";
+            _sendPinResponseDto = passedData;
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            try
+            {
+                var confirmPINResponse = await _mWService.ConfirmPinAsync(new DTO.MW.ConfirmPinRequestDto
+                {
+                    Msisdn = _sendPinResponseDto.Msisdn,
+                    SubscriptionId = _sendPinResponseDto.SubscriptionId,
+                    TransactionId = _sendPinResponseDto.TransactionId,
+                    SourceIp = clientIpAddress,
+                    Channel = "web",
+                    AdPartnerName = "MLCampaign"
+                });
 
+            }
+            catch (Exception)
+            {
+                return Page();
+            }
+            return Redirect("/Pin");
         }
     }
 }
