426c2fc22512aa6dcb9e0509955cb222c8b205eb
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-12 21:16:54 -0600

    Reformat Index

--- MobileWorldAPI/Pages/Index.cshtml.cs
+++ MobileWorldAPI/Pages/Index.cshtml.cs
@@ -13,6 +13,7 @@ namespace MobileWorldAPI.Pages
     public class IndexModel : PageModel
     {
         private readonly MWService _mWService;
+        private string clientIpAddress;
         public IndexModel(MWService mWService)
         {
             _mWService = mWService;
@@ -23,11 +24,31 @@ namespace MobileWorldAPI.Pages
         public long Msisdn { get; set; }
         public void OnGet()
         {
+            clientIpAddress = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "176.205.206.244";
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPost()
         {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                  return Page();
+                }
 
+                var sendPINResponse = await _mWService.SendPinAsync(new DTO.MW.SendPinRequestDto
+                {
+                    Msisdn = Msisdn.ToString(),
+                    SourceIp = clientIpAddress,
+                    Channel = "web",
+                    AdPartnerName = "MLCampaign"
+                });
+                return RedirectToPage("Pin", sendPINResponse);
+            }
+            catch (Exception ex)
+            {
+                return Page();
+            }
         }
     }
 }
