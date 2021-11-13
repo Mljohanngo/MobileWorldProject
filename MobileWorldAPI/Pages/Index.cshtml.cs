ff98beb10769f8b4d2b9b9cc413b750c7eac9883
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-12 20:14:21 -0600

    Add LP

--- MobileWorldAPI/Pages/Index.cshtml.cs
+++ MobileWorldAPI/Pages/Index.cshtml.cs
@@ -1,16 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MWProxy;
 
 namespace MobileWorldAPI.Pages
 {
+    
     public class IndexModel : PageModel
     {
+        private readonly MWService _mWService;
+        public IndexModel(MWService mWService)
+        {
+            _mWService = mWService;
+        }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Msisdn is required")]
+        public long Msisdn { get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }
