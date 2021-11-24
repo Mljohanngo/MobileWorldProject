2d05bc12b2b72da407c142f3086e3531beb6984a
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-24 16:05:21 -0500

    Add base class for Product entitie model

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-   public class Product 
+   public class Product : Base
    {
         public int Key { get; set; }
         public string AppName { get; set; }
