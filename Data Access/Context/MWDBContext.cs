6eec5d958b56b9b3f76f1b6a2ca51ee1e5f98be7
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-24 14:56:28 -0500

    Add DBContext class

---
+++ Data Access/Context/MWDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access.Context
+{
+    //class MWDBContext : DbContext
+    //{
+    //    public MWDBContext()
+    //    {
+
+    //    }
+
+    //    public MWDBContext(DbContextOptions<DemoWebinarDbContext> options)
+    //        : base(options)
+    //    {
+
+    //    }
+
+    //    public DbSet<Customer> Customers { get; set; }
+    //    public DbSet<Direction> Directions { get; set; }
+    //}
+}
