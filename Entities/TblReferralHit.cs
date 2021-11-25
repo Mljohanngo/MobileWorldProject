18e4b96da4d122cde30a334a38ca87c99f132f0d
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2021-11-25 10:26:55 -0500

    Add AffiliateDb contect

---
+++ Entities/TblReferralHit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Entities
+{
+    public partial class TblReferralHit
+    {
+        public long IdHit { get; set; }
+        public long IdCampaign { get; set; }
+        public string TransactionId { get; set; }
+        public string Msisdn { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string IpAddress { get; set; }
+        public string UserAgent { get; set; }
+        public string Promo { get; set; }
+    }
+}
