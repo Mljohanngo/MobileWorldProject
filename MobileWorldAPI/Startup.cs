b2a45c4c4c70572ac44f1af859db695dbf1a32ff
Mljohanngo <johann.godinez@mobilelifeconnect.com>
2022-02-13 19:01:38 -0600

    Add configuration in Startup to auto migrate db on App Start

--- MobileWorldAPI/Startup.cs
+++ MobileWorldAPI/Startup.cs
@@ -54,6 +54,12 @@ namespace MobileWorldAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<MWDBContext>();
+                context.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -73,6 +79,7 @@ namespace MobileWorldAPI
                 endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
+            
         }
     }
 }
